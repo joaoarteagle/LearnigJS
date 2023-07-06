9bf88f8f7060eb7282960cf6d19b1179bbadb663
BRUXO <joaopaulo.arteagle17@gmail.com>
2023-07-06 10:03:31 -0300

    criando pasta aulaBonieky e realizando atividades simples sobre JS

---
+++ AulaBonieky/atividade.js
@@ -0,0 +1,46 @@
+ 
+ /*
+===========VARIAVEIS===============
+
+ let carro = "Ferrari";
+ console.log(carro);
+
+ let preco = 19.90;
+ console.log(preco);
+
+ let cidade = "São Paulo";
+ console.log(cidade);
+
+ console.log("O Carro é uma " + carro + "\nO Valor do bolo é " + preco + "\nE a cidade é " + cidade);
+===================FIM==================
+
+
+
+
+=============TEMPLATE STRING===============
+let x = 0;
+
+x = x + 1;
+
+console.log(x);
+
+let nome = "João";
+let sobrenome = "Andrade";
+
+// Template String
+let nomeCompleto = `${nome} ${sobrenome}`;
+
+console.log(nomeCompleto);
+======================FIM=====================
+*/
+
+let idade = 90;
+
+if (idade>=18){
+  console.log("Você é maior de idade");
+
+}else{
+  console.log("Você é menor de Idade");
+
+}
+
