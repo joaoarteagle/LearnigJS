b1ab9f174ef68dec5bb750fe5c184f50edba19d5
JoaoAndrade <joaopaulo.arteagle17@gmail.com>
2023-07-14 18:25:33 -0300

    Aprendendo sobre funcoes e etc...

---
+++ AulaBonieky/funcoes.js
@@ -0,0 +1,15 @@
+//=========exemplo de função===========
+function nomeCompleto(nome, sobrenome){
+    return `${nome} ${sobrenome}`;
+    console.log("testando");
+}
+
+let completo = nomeCompleto("Joao", "Andrade");
+
+console.log(completo);
+//==============FIM====================
+
+
+/*Etapas de uma função */
+
+// ENTRADA -> PROCESSAMENTO -> SAÍDA
